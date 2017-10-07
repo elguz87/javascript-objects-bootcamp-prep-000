d4a7b7c9dc80ef88a07563ae10fa125c190ca581
Elliot Guzman <guleca87@gmail.com>
2017-10-07 20:34:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName){
+  playlist['Kanye']
+  return playlist
+}
