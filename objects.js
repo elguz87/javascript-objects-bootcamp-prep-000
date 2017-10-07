73db4a680981bfc5225a7ce8eb6206ff0188e4ab
Elliot Guzman <guleca87@gmail.com>
2017-10-07 20:20:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   'Dash Berlin': 'Til the sky fall down',
   'X': 'Y'
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artist] = song
+  return playlist
+}
